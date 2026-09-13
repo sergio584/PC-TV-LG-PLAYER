b2896918e84e597515859eb8c845417863ceac61
sergio584 <catoz3029@gmail.com>
2026-09-13 09:17:35 -0300

    PC to LG TV Player updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
-import { IsActiveMatchOptions, Router, RouterLink, RouterOutlet } from '@angular/router';
-import { IGX_NAVBAR_DIRECTIVES, IGX_NAVIGATION_DRAWER_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IgxSnackbarComponent, IgxToggleActionDirective } from 'igniteui-angular';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [IGX_NAVIGATION_DRAWER_DIRECTIVES, IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxToggleActionDirective, IgxIconComponent, IgxSnackbarComponent, RouterOutlet, RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    public router: Router,
-  ) {}
-
-
-  public isActive(path: string): boolean {
-    const exactMatch: IsActiveMatchOptions = {
-        paths: 'exact',
-        matrixParams: 'ignored',
-        queryParams: 'ignored',
-        fragment: 'ignored'
-    }
-    return this.router.isActive(path, exactMatch);
-  }
+  title = 'Home - IgniteUI for Angular';
 }
---
+++ src/app/master-view/master-view.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IGX_NAVIGATION_DRAWER_DIRECTIVES, IgxSnackbarComponent } from 'igniteui-angular';
+import { MasterViewComponent } from './master-view.component';
+
+describe('MasterViewComponent', () => {
+  let component: MasterViewComponent;
+  let fixture: ComponentFixture<MasterViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MasterViewComponent, NoopAnimationsModule, FormsModule, ReactiveFormsModule, RouterTestingModule, IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IGX_NAVIGATION_DRAWER_DIRECTIVES, IgxSnackbarComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MasterViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
